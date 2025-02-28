b57cb1b5645e72a4a97eecb72f0e29cd888d4e51
Andris <andrisbalogh444@gmail.com>
2025-02-28 20:56:58 +0100

    Creating env variables, deploying

--- database.js
+++ database.js
@@ -1,32 +1,16 @@
 const mysql = require('mysql');
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: null,
-    database: 'secrets'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 db.connect((err) => {
-    if(err) {
-        // If the database doesn't exists
-        if (err.code === 'ER_BAD_DB_ERROR') {
-            const newDb = mysql.createConnection({
-                host: 'localhost',
-                user: 'root',
-                password: null
-            })
-            newDb.query("CREATE DATABASE secrets", (err) => {
-                if (err) throw err;
-            })
-        };
-    };
-    console.log("Successfully connected to database")
-
-    // Creating secret table if it doesn't exists
-    db.query("CREATE TABLE IF NOT EXISTS secret(hash VARCHAR(10) PRIMARY KEY, secretText VARCHAR(255), createdAt DATETIME, expiresAt DATETIME, remainingViews INT)", (err) => {
-        if (err) throw err;
-    })
+    if(err) throw err;
+    console.log("Connected to database");
 });
 
 
