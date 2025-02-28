6fe0c8a0a0c0f8b6d2d9b67b33aabf86b910a1d8
Andris <andrisbalogh444@gmail.com>
2025-02-28 16:04:59 +0100

    Configuring database and table

--- database.js
+++ database.js
@@ -3,7 +3,31 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: null
+    password: null,
+    database: 'secrets'
 });
+
+db.connect((err) => {
+    if(err) {
+        // If the database doesn't exists
+        if (err.code === 'ER_BAD_DB_ERROR') {
+            const newDb = mysql.createConnection({
+                host: 'localhost',
+                user: 'root',
+                password: null
+            })
+            newDb.query("CREATE DATABASE secrets", (err) => {
+                if (err) throw err;
+            })
+        };
+    };
+    console.log("Successfully connected to database")
+
+    // Creating secret table if it doesn't exists
+    db.query("CREATE TABLE IF NOT EXISTS secret(hash VARCHAR(10) PRIMARY KEY, secretText VARCHAR(255), createdAt DATETIME, expiresAt DATETIME, remainingViews INT)", (err) => {
+        if (err) throw err;
+    })
+});
+
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const mysql = require('mysql');
+const db = require('./database.js');
 
 const app = express();
 
 app.listen('3000', () => {
     console.log("Server started on port 3000");
-    db.connect((err) => {
-        if(err) throw err;
-        console.log("Successfully connected to database")
-    });
 })
