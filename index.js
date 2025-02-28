aa4b9d8b564814cff72d08654a729a7a7a858a17
Andris <andrisbalogh444@gmail.com>
2025-02-28 16:31:27 +0100

    Query specific secret using hash from database

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const app = express();
 app.listen('3000', () => {
     console.log("Server started on port 3000");
 })
+
+app.get('/secret/:hash', (req, res) => {
+    const hash = req.params.hash;
+    db.query(`SELECT * FROM secret WHERE hash = ?`, [hash], (err, result)=>{
+        if (err) throw err;
+        res.send(result);
+    })
+})
