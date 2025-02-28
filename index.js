e61b0df9fe21920386d4d7fcf69026bcdde4c7da
Andris <andrisbalogh444@gmail.com>
2025-02-28 15:47:30 +0100

    Created database.js for separation

--- index.js
+++ index.js
@@ -3,17 +3,10 @@ const mysql = require('mysql');
 
 const app = express();
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: null
-});
-
-db.connect((err) => {
-    if(err) throw err;
-    console.log("Successfully connected to database")
-});
-
 app.listen('3000', () => {
     console.log("Server started on port 3000");
+    db.connect((err) => {
+        if(err) throw err;
+        console.log("Successfully connected to database")
+    });
 })
