45e4767f38a52e8debd95626c02020830b6afbb8
Andris <andrisbalogh444@gmail.com>
2025-02-28 17:02:41 +0100

    Created Secret class

---
+++ secret.js
@@ -0,0 +1,36 @@
+class Secret {
+    constructor(secret, expireAfterViews, expireAfter){
+        this.hash = (Math.trunc(Math.random() * 8999999999) + 1000000000).toString();
+        this.secret = secret;
+        this.createdAt = new Date();
+        this.expireAfter = new Date();
+        this.expireAfter.setMinutes(this.createdAt.getMinutes() + expireAfter);
+        this.expireAfterViews = Number(expireAfterViews);
+    }
+
+    get getHash(){
+        return this.hash;
+    }
+
+    get getSecret(){
+        return this.secret;
+    }
+
+    get getCreatedAt(){
+        return this.createdAt;
+    }
+
+    get getExpireAfter(){
+        return this.expireAfter;
+    }
+
+    get getExpireAfterViews(){
+        return this.expireAfterViews;
+    }
+
+    toString(){
+        return `Secret{hash: ${this.hash}, secretText: ${this.secretText}, createdAt: ${this.createdAt}, expireAfter: ${this.expireAfter}}, expireAfterViews: ${this.expireAfterViews}`;
+    }
+}
+
+module.exports = Secret;
