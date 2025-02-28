d58d812be8036f69fb270dc81e926b57bf964b99
Andris <andrisbalogh444@gmail.com>
2025-02-28 18:12:49 +0100

    Implement deletion of records with expiresAt in the past on POST/GET requests

--- index.js
+++ index.js
@@ -10,34 +10,33 @@ app.listen('3000', () => {
 })
 
 app.get('/secret/:hash', (req, res) => {
+    db.query('DELETE FROM secret WHERE expiresAt < SYSDATE()', err=>{if (err) throw err;});
     const hash = req.params.hash;
     db.query(`SELECT * FROM secret WHERE hash = ?`, [hash], (err, result)=>{
         if (err) throw err;
         if (result.length == 0) {
-            res.status(404).json({});
+            res.status(404).json({status: 404, message: "Secret not found"});
         } else {
             const currentSecret = result[0];
-            currentSecret.remainingViews -= 1;
             db.query('UPDATE secret SET remainingViews = remainingViews-1', err=>{if (err) throw err;});
             db.query('DELETE FROM secret WHERE remainingViews = 0', err=>{if (err) throw err;});
-            res.status(200).json(currentSecret);
-
-
+            return res.status(200).json({status: 200, message: "Successful operation", data: currentSecret});
         }
     })
 })
 
 app.post('/secret', (req, res) => {
-    const {secret, expireAfter, expireAfterViews} = req.body;
+    db.query('DELETE FROM secret WHERE expiresAt < SYSDATE()', err=>{if (err) throw err;});
 
+    const {secret, expireAfterViews, expireAfter} = req.body;
     if (!secret || !expireAfter || !expireAfterViews ) {
-        return res.status(405).json({})
+        return res.status(405).json({status: 405, message: "Invalid input"})
     }
 
-    const newSecret = new Secret(secret, expireAfter, expireAfterViews);
+    const newSecret = new Secret(secret, Number(expireAfterViews), Number(expireAfter));
     db.query('INSERT INTO secret (hash, secretText, createdAt, expiresAt, remainingViews) VALUES (?, ?, ?, ?, ?)',
         [newSecret.getHash, newSecret.getSecret, newSecret.getCreatedAt, newSecret.getExpireAfter, newSecret.getExpireAfterViews], (err) => {
                     if (err) throw err;
-                })
-    return res.status(200).json(newSecret)
+                });
+    return res.status(200).json({status: 200, message: "Successful operation", data: newSecret})
 })
